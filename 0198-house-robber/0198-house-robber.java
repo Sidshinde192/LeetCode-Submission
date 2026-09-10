80be92b7bc6bb8a70db950829c5fcf3c34fee330
Siddhesh Shinde <155858404+Sidshinde192@users.noreply.github.com>
2026-09-09 17:44:39 -0700

    Time: 0 ms (100%), Space: 43.1 MB (5.86%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -17,9 +17,30 @@ class Solution {
     }
     public int rob(int[] nums) {
         int n = nums.length;
-        int [] dp = new int[n];
-        Arrays.fill(dp, -1);
+        // int [] dp = new int[n];
+        // Arrays.fill(dp, -1);
 
-        return solve(0,n-1,nums, dp);
+        // return solve(0,n-1,nums, dp);
+
+        int prev2 =0;
+        int prev = nums[0];
+
+        for(int i =1; i<n;i++){
+            int take = nums[i];
+
+            if(i > 1){
+                take += prev2;
+            }
+
+            int notTake = 0 + prev;
+
+            int curr = Math.max(take, notTake);
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
+
+
     }
 }
